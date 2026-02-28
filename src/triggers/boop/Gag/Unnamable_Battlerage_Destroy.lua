48b9f91237ab078c141125344e6583ea4a4f0738
Ron <rholcomb3@gmail.com>
2026-02-27 19:09:28 -0800

    Add Unnamable Destroy gag trigger

---
+++ src/triggers/boop/Gag/Unnamable_Battlerage_Destroy.lua
@@ -0,0 +1,7 @@
+-- Generated from observed Unnamable battlerage line.
+boop.gag.onAttackLine({
+  source = "Unnamable/Battlerage/Destroy.lua",
+  ability = "Destroy",
+  actor = { kind = "literal", value = "You" },
+  target = { kind = "match", index = 2 },
+}, matches, line or "")
